be0568f78cb8fc7bdaa87fbd5d2b21b70c01442f
ThywillJoshua <76951731+ThywillJoshua@users.noreply.github.com>
2022-04-11 16:51:15 +0200

    Create quiz page and result page

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Quiz from "./Quiz";
+import App from "./App";
+import Nav from "./components/Nav";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Quiz />
+    <Nav />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
