0b5406ed80cf80057226466fb8adc7b4d8d1dbbc
ThywillJoshua <76951731+ThywillJoshua@users.noreply.github.com>
2022-04-11 13:32:52 +0200

    Add page and component folder, create landingPage and Nav component

---
+++ src/Quiz.tsx
@@ -0,0 +1,15 @@
+import "./Quiz.css";
+
+import Nav from "./components/Nav";
+import LandingPage from "./pages/LandingPage";
+
+function App() {
+  return (
+    <div className="container">
+      <Nav />
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,38 @@
+import "./LandingPage.css";
+import laying from "../laying.svg";
+import selfie from "../selfie.svg";
+
+export default function LandingPage() {
+  return (
+    <section className="section-hero">
+      <div className="hero-message">
+        <h1>Are you an Introvert or an Extrovert?</h1>
+
+        <p>
+          Sometimes we wonder how people percieve us, if you really want to know
+          what they think, take the test below and you won’t be curious anymore.
+          The test is proven an has been tried and tested.
+        </p>
+
+        <button className="btn">
+          <span>Take a quick test!</span>
+          <span>Time: 5 minutes</span>
+        </button>
+      </div>
+
+      <div className="hero-illustrations">
+        <img
+          src={selfie}
+          alt="A man taking a selfie"
+          className="illustration-selfie"
+        />
+
+        <img
+          src={laying}
+          alt="A woman laying and reading a book"
+          className="illustration-laying"
+        />
+      </div>
+    </section>
+  );
+}
