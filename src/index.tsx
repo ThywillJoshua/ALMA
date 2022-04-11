5d4c231444ac66572f0b2a7d449501733b16ab86
ThywillJoshua <76951731+ThywillJoshua@users.noreply.github.com>
2022-04-11 23:03:57 +0200

    Create question and answer flow

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Nav from "./components/Nav";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import QuizPage from "./pages/QuizPage";
-import ResultPage from "./pages/ResultPage";
+import { BrowserRouter } from "react-router-dom";
+import { QuizProvider } from "./contexts/quizContext";
 
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/quiz" element={<QuizPage />} />
-        <Route path="/result" element={<ResultPage />} />
-      </Routes>
+      <QuizProvider>
+        <App />
+      </QuizProvider>
     </React.StrictMode>
   </BrowserRouter>,
   document.getElementById("root")
