35d9590b1e46ee08c3f547b497c01a8ddb974c04
ThywillJoshua <76951731+ThywillJoshua@users.noreply.github.com>
2022-04-13 23:03:57 +0200

    Remove App.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import Nav from "./components/Nav";
+import "./styles/global.scss";
 
+import Nav from "./components/Nav";
 import LandingPage from "./pages/LandingPage";
 import QuizPage from "./pages/QuizPage";
 import ResultPage from "./pages/ResultPage";
